8ddfa3847e3582490b9fbe0ef0e31bbf6816cd36
Antoopizarro <antonella_pizarro@yahoo.com.ar>
2022-09-27 22:12:04 -0300

    emepzando tp

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,20 @@
+const db = require('../database/models');
+
+module.exports = {
+    list : (req, res)=>{
+        db.Movie.findAll()
+            .then((movies) => {
+                return res.send(movies)
+            })
+            .catch(error => console.log(error))
+    },
+    new : (req,res) => {
+
+    },
+    recommended : (req,res)=> {
+
+    },
+    detail : (req,res) => {
+        
+    }
+}
