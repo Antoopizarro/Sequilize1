dec57d0b32efaeac9153178ea7d3dc584105c0f9
Antoopizarro <antonella_pizarro@yahoo.com.ar>
2022-09-28 00:14:05 -0300

    agregue modelo de actores

--- src/controllers/actorsController.js
+++ src/controllers/actorsController.js
@@ -2,9 +2,17 @@ const db = require('../database/models');
 
 module.exports = {
     list : (req, res) => {
-
+        db.Actor.findAll()
+            .then((actors) => {
+                return res.render('actorsList', {
+                    actors
+                })
+            })
+            .catch(error => console.log(error))
     },
     detail : (req,res) => {
-        
+        db.Actor.findByPk(req.params.id)
+            .then(movie => res.render('actorsDetail'), {actor})
+            .catch(error => console.log(error))
     }
 }
--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -11,10 +11,21 @@ module.exports = {
             .catch(error => console.log(error))
     },
     new : (req,res) => {
-
+        const queryOps = {
+            order : [['release_date', 'DESC']]
+        };
+        db.Movie.findAll(queryOps)
+            .then(movies => res.render('moviesList', {movies}))
+            .catch(error => console.log(error))
     },
     recommended : (req,res)=> {
-
+        const queryOps = {
+            order : [['release_date', 'DESC']],
+            limit : 5
+        }
+        db.Movie.findAll(queryOps)
+            .then(movies => res.render('moviesList', {movies}))
+            .catch(error => console.log(error))
     },
     detail : (req,res) => {
         db.Movie.findByPk(req.params.id)
